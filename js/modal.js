253423a48744cc30fea2178c33e1c3337eff4720
JamilTalukder103 <mdjamiltalukder143@gmail.com>
2024-12-08 20:06:04 +0600

    modal add

---
+++ js/modal.js
@@ -0,0 +1,8 @@
+function openModal() {
+    document.getElementById('success-modal').classList.add('modal-open');
+}
+
+
+function closeModal() {
+    document.getElementById('success-modal').classList.remove('modal-open');
+}
